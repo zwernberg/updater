9bccefbc1f76df5eacdd232382fdc3b85a62ecfe
Zach Wernberg <zawerny@gmail.com>
2017-12-19 20:33:15 -0600

    add back debug loggin

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  console.log('user connected');
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
