5477fb6f07a0c673692bd7c1adcb6c87e70a0c54
Zach Wernberg <zawerny@gmail.com>
2017-12-19 20:26:58 -0600

    websockets!

---
+++ index.js
@@ -0,0 +1,35 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+const axios = require('axios');
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  axios.get('http://schumacher.football/api/championship/')
+  .then(response => {
+    socket.emit('update', response.data)
+  })
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+	console.log('message:' + msg);
+  });
+  setInterval(function() {
+    if (io.engine.clientsCount > 0) {
+      axios.get('http://schumacher.football/api/championship/')
+        .then(response => {
+          socket.emit('update', response.data)
+        })
+    }
+  }, 1000 * 60);
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
